794d08db4e2c765830ae4a34d4c5028dd6e84ff3
ermi-mekdi <129061677+ermi-mekdi@users.noreply.github.com>
2025-09-07 23:09:22 +0200

    data folder 2 file  func plc

--- script.js
+++ script.js
@@ -20,7 +20,31 @@ function displayPerson(m) {
   <h3>${m.info}</h3>
   `;
 }
+function disPlc(p) {
+  const display = document.createElement("div");
+  display.classList.add("person");
+  display.id = "pdisplay";
+  document.body.appendChild(display);
 
+  const def1 = p.nameM1 ? p.name1 + " ማለት " + p.nameM1 : "";
+  const def2 = p.nameM2 ? p.name2 + " ማለት " + p.nameM2 : "";
+  const vers = p.vers ? p.vers : "";
+  const gMap = p.gMap
+    ? `<a href="${p.gMap}" target="_blank">Google Map</a>`
+    : "";
+  const info = p.info ? p.info : "";
+
+  display.innerHTML = `
+  <div onclick="de()" class="x">X</div>
+ 
+  <h2> ${p.name1} (${p.nameE1})  ${p.name2}  ${p.nameE2} </h2><br>
+  <h3>${def1}</h3><br>
+  <h3>${def2} </h3><br>
+  <h3>${vers}</h3><br>
+  <h3>${gMap}</h3><br>
+  <h3>${info}</h3>
+  `;
+}
 function de() {
   const display = document.getElementById("pdisplay").remove();
 }
