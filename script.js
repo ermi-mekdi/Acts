06487b11a0f5c0962fbfc177c0e1d219d8f7e1e0
ermi-mekdi <129061677+ermi-mekdi@users.noreply.github.com>
2025-10-12 23:16:50 +0200

    bewe 3

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ function displayPerson(m) {
   display.id = "pdisplay";
   document.body.appendChild(display);
   const sex = m.man === true ? "&#128104; " : "&#128105; ";
-  const def1 = m.nameM1 ? m.name1 + " = " + m.nameM1 : "";
+  const def1 = m.nameM1 ? m.name1 + " ማለት " + m.nameM1 : "";
   const def2 = m.nameM2 ? m.name2 + " ማለት " + m.nameM2 : "";
   const naam2 = m.name2 ? `ካልኣይ ስም  ${m.name2}  (${m.nameE2})` : "";
 
   display.innerHTML = `
   <div onclick="de()" class="x">X</div>
   ${sex} 
-  <h2> ስም ${m.name1} (${m.nameE1}) </h2><br>
-  <h2> ${naam2} </h2><br>
-  <h3>${def1}</h3><br>
-  <h3>${def2} </h3><br>
-  <h3>${m.title}</h3><br>
-  <h3>${m.adres}</h3><br>
+  <h2> ስም ${m.name1} (${m.nameE1}) </h2>
+  <h2> ${naam2} </h2>
+  <h3>${def1}</h3>
+  <h3>${def2} </h3>
+  <h3>${m.title}</h3>
+  <h3>${m.adres}</h3>
   <h3>${m.info}</h3>
   <button class="xbtn" onclick="de()">Close</button>
   `;
@@ -39,11 +39,11 @@ function disPlc(p) {
   display.innerHTML = `
   <div onclick="de()" class="x">X</div>
  
-  <h2> ${p.name1} (${p.nameE1})  ${p.name2}  ${p.nameE2} </h2><br>
-  <h3>${def1}</h3><br>
-  <h3>${def2} </h3><br>
-  <h3>${vers}</h3><br>
-  <h3>${gMap}</h3><br>
+  <h2> ${p.name1} (${p.nameE1})  ${p.name2}  ${p.nameE2} </h2>
+  <h3>${def1}</h3>
+  <h3>${def2} </h3>
+  <h3>${vers}</h3>
+  <h3>${gMap}</h3>
   <h3>${info}</h3>
   <button class="xbtn" onclick="de()">Close</button>
   `;
